53d8f38d339eff52620ce66824e6974659d6ccd5
Sharath Kumar U B <sharathkumarub.97@gmail.com>
2024-04-25 21:32:08 +0530

    Own DB commit everywhere

--- src/main/java/org/sharath/app/productmanagementproj/models/BaseModel.java
+++ src/main/java/org/sharath/app/productmanagementproj/models/BaseModel.java
@@ -1,2 +1,19 @@
-package org.sharath.app.productmanagementproj.models;public class BaseModel {
+package org.sharath.app.productmanagementproj.models;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseModel {
+    @Id
+    private Long id;
+    private Date createdAt;
+    private Date updatedAt;
+    private boolean isDeleted;
 }
